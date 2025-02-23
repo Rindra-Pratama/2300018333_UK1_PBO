cf53539107433f14e0b36360016a96761c3cc297
Muhammad Rindra <muhammadrindra11@gmail.com>
2025-02-23 15:30:16 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Instansiasi objek BukuFiksi
+        
         BukuFiksi bukuFiksi = new BukuFiksi("Harry Potter", "J.K. Rowling", "Fantasy");
         bukuFiksi.infoBuku();
 
