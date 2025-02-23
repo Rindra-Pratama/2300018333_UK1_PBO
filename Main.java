4a564e32b2c5061fb65ce51db84865015050172c
Muhammad Rindra <muhammadrindra11@gmail.com>
2025-02-23 15:30:31 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main {
         BukuFiksi bukuFiksi = new BukuFiksi("Harry Potter", "J.K. Rowling", "Fantasy");
         bukuFiksi.infoBuku();
 
-        // Instansiasi objek BukuNonFiksi
+       
         BukuNonFiksi bukuNonFiksi = new BukuNonFiksi("Sejarah Dunia", "John Doe", "Sejarah");
         bukuNonFiksi.infoBuku();
 
-        // Instansiasi objek Petugas
+
         Petugas petugas = new Petugas("Budi", "P001", "Siang");
         petugas.infoAnggota();
         petugas.melayani();
 
-        // Polymorphism - Menggunakan referensi Buku
+   
         Buku bukuPoli = new BukuFiksi("The Hobbit", "J.R.R. Tolkien", "Adventure");
-        bukuPoli.infoBuku(); // Memanggil method override dari BukuFiksi
+        bukuPoli.infoBuku(); 
     }
 }
